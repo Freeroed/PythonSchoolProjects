cac46b59c22881eee6ab18d7519362355f879049
Freeroed <Telekines1551@gmail.com>
2020-01-06 23:00:05 +0300

    Добавлен спавн мячика

--- PyGame/Pong.py
+++ PyGame/Pong.py
@@ -24,9 +24,9 @@ class ball:
     #Проверка вылета за границу
     def check_range(self):
         if self.x + self.speed_x>window_w-self.radius-5:
-            self.speed_x*=-1 * self.change
+            self.speed_x*=-1 * self.change 
         if self.x + self.speed_x<0+self.radius+5:
-            self.speed_x*=-1 * self.change
+            self.speed_x*=-1 * self.change 
         if self.y + self.speed_y<0+self.radius+5:
             self.speed_y *=-1 * self.change
         if self.y + self.speed_y>window_h-self.radius-5:
@@ -37,18 +37,26 @@ class ball:
         self.x =round(self.x + self.speed_x)
         self.y =round(self.y + self.speed_y)
         self.check_range()
+    
+    def spawnBall(self, w, h):
+        self.x = w//2
+        self.y = h//2
+        self.speed_x = random.randint(-20,20)
+        self.speed_y = random.randint(-20,20)
 
 run = True
 
-bl = ball(window_w//2, window_h//2, 10, (255,0,0), random.randint(-20,20),random.randint(-20,20), 1.1)
+bl = ball(window_w//2, window_h//2, 10, (255,0,0), random.randint(-20,20),random.randint(-20,20), 1)
 #Основной цикл программы
 while run:
     clock.tick(30)
-
+    #Проверяем нажатые клавиши
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    if keys[pygame.K_SPACE]:
+        bl.spawnBall(window_w,window_h)
     window.fill((0,0,0))
     pygame.draw.rect(window, (100,0,255), (0,0, 5, window_h))
     pygame.draw.rect(window, (100,0,255), (0,0, window_w, 5))
