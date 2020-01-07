936ee92c20412ba16c37204472b472a018f0c950
Freeroed <Telekines1551@gmail.com>
2020-01-07 20:44:54 +0300

    Змея доведена до ума. Добавлена возможность умереть Настроена генерация еды Настроено прохождение сквозь стены

--- PyGame/Snake.py
+++ PyGame/Snake.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint, randrange
 
 class snake(object):
     def __init__(self, segments,speed_x,speed_y):
@@ -12,7 +13,12 @@ class snake(object):
         self.head.draw(wind)
         for segmen in self.segments:
             segmen.draw(wind)
-        
+    def checking(self):
+        answer = True
+        for i in range(2,len(self.segments)-1):
+            if not self.segments[0].check_contakt(self.segments[i]):
+                return False
+        return answer
 
     def update(self):
         for i in range(len(self.segments)-1, 0, -1):
@@ -20,15 +26,25 @@ class snake(object):
             self.segments[i].y = self.segments[i-1].y
         self.segments[0].x += self.speed_x
         self.segments[0].y += self.speed_y
+    def add(self):
+        temp = self.segments[-1]
+        self.segments.append(segment(temp.x,temp.y,temp.color))
 class segment(object):
     def __init__(self, x,y,colour):
         self.x=x
         self.y=y
         self.color = colour
 
+    
 
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), 10)
+
+    def check_contakt(self, other):
+        answer = True
+        if (self.x == other.x and self.y == other.y):
+            answer = False
+        return answer
 
 #Умираем если трогаем границу
 def chek_dead(snake_head, win_w, win_h):
@@ -41,15 +57,30 @@ def chek_dead(snake_head, win_w, win_h):
 
 #Если дотрагиваемся до границы, телепортируемся
 def teleport(snake_head, win_w, win_h, spd_x,spd_y):
-    if snake_head.x-10+spd_x<0:
+    if snake_head.x-10<0:
         snake_head.x = win_w-10
-    elif snake_head.x+10+spd_x>win_w: 
+    elif snake_head.x>win_w: 
         snake_head.x = 0+10
-    elif snake_head.y-10+spd_y<0: 
+    elif snake_head.y-10<0: 
         snake_head.y = win_h-10
-    elif snake_head.y+10+spd_y>win_h: 
+    elif snake_head.y+10>win_h: 
         snake_head.y = 0+10
     return snake_head
+
+
+def check_food_spawn(x,y,snk):
+    for snake_seg in snk.segments:
+        if (x==snake_seg.x and y == snake_seg.y):
+            return False
+    return True
+
+def generate_food(snk):
+    x = randrange(1,window_w//20,2)*10
+    y = randrange(1,window_h//20,2)*10
+    while not check_food_spawn(x,y,snk):
+        x = randrange(1,window_w//20,2)*10
+        y = randrange(1,window_h//20,2)*10
+    return segment(x,y,(0,0,255))
 #Настройки окна
 window_w = 500
 window_h = 500
@@ -58,11 +89,18 @@ clock = pygame.time.Clock()
 run = True
 tick = 1
 game_speed=1
+
+#Длинна и полжение змеи вначале
 head = segment(70,50, (255,0,0))
 seg = segment(50,50, (255,255,255))
 seg2 = segment(50,30, (255,255,0))
 seg3 = segment(50,10, (255,255,0))
 sn = snake([head,seg,seg2, seg3],20,0)
+
+#Начальная генерация еды
+food = generate_food(sn)
+
+#Основной цикл игры
 while run:
     clock.tick(30)
     pygame.time.delay(10)
@@ -71,25 +109,30 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and sn.speed_y==0:
-        sn.speed_x=0
-        sn.speed_y=20
-    if (keys[pygame.K_UP] or keys[pygame.K_w]) and sn.speed_y==0:
-        sn.speed_x=0
-        sn.speed_y=-20
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a])and sn.speed_x==0:
-        sn.speed_x=-20
-        sn.speed_y=0
-    if (keys[pygame.K_RIGHT] or keys[pygame.K_d])and sn.speed_x==0:
-        sn.speed_x=20
-        sn.speed_y=0
-    sn.segments[0]=teleport(sn.segments[0], window_w, window_h, sn.speed_x, sn.speed_y)
-    if tick%10 == 0 :
+    
+    #sn.segments[0]=teleport(sn.segments[0], window_w, window_h, sn.speed_x, sn.speed_y)
+    if tick%3 == 0 :
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and sn.speed_y==0:
+            sn.speed_x=0
+            sn.speed_y=20
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and sn.speed_y==0:
+            sn.speed_x=0
+            sn.speed_y=-20
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a])and sn.speed_x==0:
+            sn.speed_x=-20
+            sn.speed_y=0
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d])and sn.speed_x==0:
+            sn.speed_x=20
+            sn.speed_y=0
+        if (keys[pygame.K_g]):
+            sn.add()
         window.fill((0,0,0))
         sn.draw(window)
-        #run = chek_dead(sn.segments[0],window_w, window_h)
-        
-        print("Update")
+        food.draw(window)
+        run = sn.checking()
+        run = chek_dead(sn.segments[0],window_w, window_h)
+        if not(sn.segments[0].check_contakt(food)):
+            food = generate_food(sn)
+            sn.add()
     pygame.display.update()
-    print (tick)
     tick+=1
